e02c25af90d9c53c164f0cf23d82d8cf4d55ac90
WonderWoman <74731481+WonderWoman2020@users.noreply.github.com>
2024-01-07 05:12:54 +0100

    Added CRUD methods for Record in RecordService, testing required

--- BTreeIndex/src/main/java/record/service/RecordService.java
+++ BTreeIndex/src/main/java/record/service/RecordService.java
@@ -8,6 +8,7 @@ import record.entity.Record;
 import tape.service.TapeService;
 
 import java.io.File;
+import java.nio.ByteBuffer;
 import java.security.InvalidAlgorithmParameterException;
 import java.util.*;
 
@@ -164,6 +165,182 @@ public class RecordService {
             return true;
 
         return false;
+    }
+
+    // Non-sequential read/write methods for Record
+    /**
+     * This method assumes that upper layer takes responsibility of freeing buffers for tapes, if they're full.
+     * It also assumes, that a record must be written as a whole on a single page. It assumes too, that records
+     * are written one by one and there are no gaps - writeRecord() function should be responsible for
+     * shifting them that way, when removing or adding a record, before the page will be saved on disk.
+     * @param tapeID
+     * @param page
+     * @param key
+     * @return Requested record or null, if the record with key equal to provided one hasn't been found on this page.
+     */
+    public Record readRecord(UUID tapeID, int page, long key)
+    {
+        if(!tapeService.getBufferedPages(tapeID).contains(page) && tapeService.isMaxBuffers(tapeID))
+            throw new IllegalStateException("Reading requested record requires loading a page from tape, but the buffer" +
+                    " limit for this tape is full. Some buffer should have been freed before requesting this operation.");
+
+        if(page < 0)
+            throw new IllegalStateException("Page can't be a negative number.");
+
+        byte[] buffer = tapeService.readPage(tapeID, page);
+        if(buffer == null)
+            throw new IllegalStateException("Requested page should exist, but its data was null.");
+
+        int pos = this.findRecordPosition(buffer, key);
+        if(pos == -1)
+            return null; // Record not found
+
+        return recordConverter.bytesToRecord(buffer, pos);
+    }
+
+    /**
+     * Writes provided record to requested page buffer and saves the page on disk. Requires loading the page on which
+     * record will be stored, so number of free buffers needs to be checked beforehand to contain at least 1 buffer spot.
+     * @param tapeID
+     * @param page
+     * @param record
+     * @throws InvalidAlgorithmParameterException
+     */
+    public void createRecord(UUID tapeID, int page, Record record) throws InvalidAlgorithmParameterException {
+        if(!tapeService.getBufferedPages(tapeID).contains(page) && tapeService.isMaxBuffers(tapeID))
+            throw new IllegalStateException("Creating requested record requires loading a page from tape, but the buffer" +
+                    " limit for this tape is full. Some buffer should have been freed before requesting this operation.");
+
+        if(page < 0)
+            throw new IllegalStateException("Page can't be a negative number.");
+
+        if(record == null)
+            throw new IllegalStateException("Record provided to write was null. Creation of the record aborted.");
+
+        byte[] buffer = tapeService.readPage(tapeID, page);
+        if(buffer == null)
+            throw new IllegalStateException("Requested page should exist, but its data was null.");
+
+        if(this.findRecordPosition(buffer, record.getKey()) != -1)
+            throw new IllegalStateException("Record with that key already exists on this page." +
+                    " A key of a record should be unique for a whole tape.");
+
+        int freeSpaceStart = this.findStartOfFreeSpace(buffer);
+        if(freeSpaceStart == -1 || buffer.length - freeSpaceStart < record.getSize())
+            throw new IllegalStateException("There is not enough space on this page to store new record on it.");
+
+        recordConverter.recordToBytes(record, buffer, freeSpaceStart);
+        tapeService.writePage(tapeID, page, buffer, buffer.length);
+    }
+
+    /**
+     * This method assumes, that all records are of the same size (which is true for my type of Record for this project).
+     * @param tapeID
+     * @param page
+     * @param record
+     */
+    public void updateRecord(UUID tapeID, int page, Record record) throws InvalidAlgorithmParameterException {
+        if(!tapeService.getBufferedPages(tapeID).contains(page) && tapeService.isMaxBuffers(tapeID))
+            throw new IllegalStateException("Creating requested record requires loading a page from tape, but the buffer" +
+                    " limit for this tape is full. Some buffer should have been freed before requesting this operation.");
+
+        if(page < 0)
+            throw new IllegalStateException("Page can't be a negative number.");
+
+        if(record == null)
+            throw new IllegalStateException("Record provided to write was null. Creation of the record aborted.");
+
+        byte[] buffer = tapeService.readPage(tapeID, page);
+        if(buffer == null)
+            throw new IllegalStateException("Requested page should exist, but its data was null.");
+
+        int pos = this.findRecordPosition(buffer, record.getKey());
+        if(pos == -1)
+            throw new IllegalStateException("Record with that key doesn't exists on this page, so it can't be updated.");
+
+        if(buffer.length - pos < record.getSize())
+            throw new IllegalStateException("Since the record to update was found on this page, there should be enough" +
+                    " space for the updating record (all records of type Record are the same size), but it wasn't.");
+
+        recordConverter.recordToBytes(record, buffer, pos);
+        tapeService.writePage(tapeID, page, buffer, buffer.length);
+    }
+
+    public void removeRecord(UUID tapeID, int page, long key) throws InvalidAlgorithmParameterException {
+        if(!tapeService.getBufferedPages(tapeID).contains(page) && tapeService.isMaxBuffers(tapeID))
+            throw new IllegalStateException("Creating requested record requires loading a page from tape, but the buffer" +
+                    " limit for this tape is full. Some buffer should have been freed before requesting this operation.");
+
+        if(page < 0)
+            throw new IllegalStateException("Page can't be a negative number.");
+
+        byte[] buffer = tapeService.readPage(tapeID, page);
+        if(buffer == null)
+            throw new IllegalStateException("Requested page should exist, but its data was null.");
+
+        int pos = this.findRecordPosition(buffer, key);
+        if(pos == -1)
+            throw new IllegalStateException("Record with that key doesn't exists on this page, so it can't be removed.");
+
+        Record record = recordConverter.bytesToRecord(buffer, pos);
+        if(record == null)
+            throw new IllegalStateException("Record with provided key was found on the page, but after trying to read" +
+                    " it, it turned out null. Something went wrong, removing the record aborted.");
+
+        // Erase the record
+        Arrays.fill(buffer, pos, pos + record.getSize(), (byte) 0);
+        // Shift all bytes (possible records) from right to the pos, to remove gap between records
+        System.arraycopy(buffer, pos + record.getSize(), buffer, pos, buffer.length - pos);
+        // Erase old bytes at the end of array, that stayed he same (there could be a redundant copy of a
+        // record, that has been shifted 1 spot left)
+        Arrays.fill(buffer, buffer.length - record.getSize(), buffer.length, (byte) 0);
+        
+        tapeService.writePage(tapeID, page, buffer, buffer.length);
+    }
+
+    /**
+     * Gets a buffer and searches for the requested record position in it.
+     * @param buffer
+     * @param key
+     * @return Position of the record in the buffer, if found, or -1, if not.
+     */
+    public int findRecordPosition(byte[] buffer, long key)
+    {
+        if(buffer == null)
+            throw new IllegalStateException("Provided page data buffer was null.");
+
+        int consumed = 0;
+        while(recordConverter.isFullRecord(buffer, consumed))
+        {
+            Record record = recordConverter.bytesToRecord(buffer, consumed);
+            if(record.getKey() == 0) // Assuming end of records on that page, key equal to 0 is forbidden
+                return -1;
+
+            if(record.getKey() == key)
+                return consumed;
+
+            consumed += record.getSize();
+        }
+        // Since this method assumes only a whole record could be on one page, now the whole page is assumed to be read
+        return -1;
+    }
+
+    public int findStartOfFreeSpace(byte[] buffer)
+    {
+        if(buffer == null)
+            throw new IllegalStateException("Provided page data buffer was null.");
+
+        int consumed = 0;
+        while(recordConverter.isFullRecord(buffer, consumed))
+        {
+            Record record = recordConverter.bytesToRecord(buffer, consumed);
+            if(record.getKey() == 0) // Assuming end of records on that page, key equal to 0 is forbidden
+                return consumed;
+
+            consumed += record.getSize();
+        }
+        // Since this method assumes only a whole record could be on one page, now the whole page is assumed to be read
+        return -1;
     }
 
     // Some TapeService methods, which are needed in upper app layers (This is an attempt to achieve encapsulation,
