70513ffd088d8e9936cfadca7cf29201ca342210
WonderWoman <74731481+WonderWoman2020@users.noreply.github.com>
2024-01-08 05:23:36 +0100

    Added Statistics entity class

---
+++ BTreeIndex/src/main/java/statistics/entity/Statistics.java
@@ -0,0 +1,38 @@
+package statistics.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class Statistics {
+
+    /**
+     * Operation number. It is counted for each operation type separately.
+     */
+    private long operation;
+
+    /**
+     * Database operation types. Possible types: CREATE, READ, UPDATE, DELETE.
+     */
+    private String type;
+
+    // Counters of things that happened during operation
+
+    private int merges;
+
+    private int splits;
+
+    private int compensations;
+
+    private int unCompensations;
+
+    private int tapeWrites;
+
+    private int tapeReads;
+
+
+}
